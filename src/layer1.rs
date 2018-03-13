5b727f4d378b7e83b4c480fd1d2803dba93fde8a
Niko Matsakis <niko@alum.mit.edu>
2018-03-13 07:24:56 -0400

    add some comments and cleanup

--- src/layer1.rs
+++ src/layer1.rs
@@ -3,10 +3,26 @@ use std::mem;
 use crate::drop_thunk;
 use crate::{FreeSuspended, Suspend};
 
+/// Defines the "closed" -- or "suspended" -- version of a reference
+/// with one existential lifetime. In this version, the existential
+/// lifetime we plan to re-open does not appear.
+///
+/// A `Close1` is always tied to an `Open1`, which defines the version
+/// of the type where the existential lifetime *does* appear. You can
+/// use the `Opened1<'a, Self>` alias to access that type.
+///
+/// Every `Close1` has a defined "base type" B from which it can be
+/// constructed; to create one, you invoke `layer_on` with an instance
+/// of this base type B. References with the existential lifetime are
+/// (typically, anyway) referring into this base value (which is moved
+/// into the heap, so that it has a stable address).
+///
+/// The actual construction of the `Close1` is done by the `build`
+/// method, which is internal to `Close1`.
 pub trait Close1: FreeSuspended + for<'a> Open1<'a> {
     type Input;
 
-    fn build<'a>(input: &'a Self::Input) -> <Self as Open1<'a>>::Output;
+    fn build<'a>(input: &'a Self::Input) -> Opened1<'a, Self>;
 
     fn layer_on<'bound>(value: Self::Input) -> Suspend<'bound, Self>
     where
@@ -49,6 +65,9 @@ pub trait Open1<'a> {
 
 pub type Opened1<'a, L> = <L as Open1<'a>>::Output;
 
+/// Defines a closure that takes as input a "opened" layer-1 value.
+/// The only reason this trait exists (versus a normal closure) is to
+/// workaround normalization bugs in rustc.
 pub trait Func1<Input: Close1> {
     type Output;
 
