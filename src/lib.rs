6aed80ec87d3817eecc0a4047e97ddba720e30ca
Niko Matsakis <niko@alum.mit.edu>
2018-03-13 07:02:03 -0400

    kill `Suspend0`

--- src/lib.rs
+++ src/lib.rs
@@ -12,41 +12,6 @@ mod test;
 pub mod layer1;
 
 use crate::drop_thunk::DropThunk;
-use crate::layer1::{Close1, Opened1};
-
-use std::mem;
-
-pub struct Suspend0<T> {
-    data: Box<T>,
-}
-
-impl<T> Suspend0<T> {
-    pub fn new(value: T) -> Suspend0<T> {
-        Suspend0 {
-            data: Box::new(value),
-        }
-    }
-
-    pub fn open<R>(&mut self, op: impl for<'a> FnOnce(&'a mut T) -> R) -> R {
-        op(&mut self.data)
-    }
-
-    pub fn layer<'bound, L>(self) -> Suspend1<'bound, L>
-    where
-        L: Close1<Input = T>,
-        T: 'bound,
-    {
-        unsafe {
-            let (data_ptr, drop_thunk) = drop_thunk::split_box(self.data);
-            let open_data: Box<Opened1<'_, L>> = Box::new(L::build(&*data_ptr));
-            let closed_data: Option<Box<L>> = Some(mem::transmute(open_data));
-            Suspend1 {
-                closed_data,
-                drop_thunk,
-            }
-        }
-    }
-}
 
 pub struct Suspend1<'bound, L: FreeSuspended> {
     closed_data: Option<Box<L>>,
